ae4c19b061c0fd6236d17e12f75ca4019c861a7d
Florian Nitschmann <f.nitschmann@googlemail.com>
2016-08-08 16:48:08 +0200

    more preparations for test env

--- rails-settings-manager.gemspec
+++ rails-settings-manager.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "pry-rails"
+  spec.add_development_dependency "sqlite3"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,76 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rails/settings/manager'
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
+
+require "rspec"
+require "rails/all"
+require "sqlite3"
+require "pry"
+
+require "settings-manager"
+
+if SettingsManager::Base.respond_to?(:raise_in_transactional_callbacks=)
+  SettingsManager::Base.raise_in_transactional_callbacks = false
+end
+
+class TestApplication < Rails::Application ; end
+
+module Rails
+  class << self
+    def cache
+      @cache ||= ActiveSupport::Cache::MemoryStore.new
+    end
+
+    def env
+      "test"
+    end
+
+    def root
+      Pathname.new(File.expand_path("../", __FILE__))
+    end
+  end
+end
+
+# SQLite3 in memory
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "memory"
+)
+
+# migrations
+ActiveRecord::Schema.verbose = false
+ActiveRecord::Schema.define(:version => 1) do
+  # settings table
+  if ActiveRecord::Base.connection.table_exists?(:settings)
+    drop_table :settings
+  end
+
+  create_table :settings do |t|
+    t.string  :key,           :null => false
+    t.text    :value,         :null => true
+    t.integer :base_obj_id,   :null => true
+    t.string  :base_obj_type, :null => true
+
+    t.timestamps :null => false
+  end
+
+  # users table
+  if ActiveRecord::Base.connection.table_exists?(:users)
+    drop_table :users
+  end
+
+  create_table :users do |t|
+    t.string :username, :null => false
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:all) do
+    class Setting < SettingsManager::Base ; end
+
+    ActiveRecord::Base.connection.execute('delete from settings')
+    Rails.cache.clear
+  end
+
+  config.after(:all) { Object.send(:remove_const, :Setting) }
+end
+
+Rails.application.instance_variable_set("@initialized", true)
